1740d65b4d00c6fd138458ade49ba5ce6ca567e3
GitTinOut <ttl052@ucsd.edu>
2018-04-26 04:08:27 -0700

    refactored to include projection optimization

--- knn.py
+++ knn.py
@@ -4,41 +4,73 @@ from scipy.stats import mode
 
 class KNN:
     def __init__(self, train, valid, test, k):
-        self.train = pd.read_csv(train,sep = ' ').as_matrix()
-        self.valid = pd.read_csv(valid, sep = ' ').as_matrix()
-        self.test = pd.read_csv(test, sep = ' ').as_matrix()
+        self.trainfile = train
+        self.validfile = valid
+        self.testfile = test
+        self.train = pd.read_csv(train,sep = ' ',header = None).as_matrix()
+        self.valid = pd.read_csv(valid, sep = ' ',header = None).as_matrix()
+        self.test = pd.read_csv(test, sep = ' ', header = None).as_matrix()
         self.k = k
 
     def setk(self,new):
         self.k = new
+    
+    """sets the projected training matrix by matrix multiplication"""
+    def setp(self, p):
+        self.ptrain = np.matmul( np.delete(pd.read_csv(self.trainfile, sep = ' ',header = None).as_matrix(), -1, axis=1),
+                                 pd.read_csv(p, sep = ' ', header = None).as_matrix() )
+        self.pvalid = np.matmul( np.delete(pd.read_csv(self.validfile, sep = ' ',header = None).as_matrix(), -1, axis=1),
+                                 pd.read_csv(p, sep = ' ', header = None).as_matrix() )
+        self.ptest = np.matmul( np.delete(pd.read_csv(self.testfile, sep = ' ',header = None).as_matrix(), -1, axis=1),
+                                 pd.read_csv(p, sep = ' ', header = None).as_matrix() )
 
-    def error(self, test, size):
+    def error(self, test, orig, size, proj):
         wrong = 0
+        if proj == False:
+            train = self.train
+        else:
+            train = self.ptrain
+
         """ predict the label of each point in the training dataset using that training dataset"""
         for i in range(size):
-            if self.predict(self.train, test[i]) == False:
+            if self.predict(train, test[i], proj,i, orig) == False:
                 wrong += 1
         accuracy = wrong/size
         return accuracy
 
-    def predicttrain(self):
-        print("Training Error:" + str(self.error(self.train, 1999)))
-    
-    def predictvalid(self):
-        print("Validation Error:"+ str(self.error(self.valid, 999)))
-    
-    def predicttest(self):
-        print("Test Error: " + str(self.error(self.test, 999)))
+    def predicttrain(self, proj = False):
+        if proj == False:
+            print("Training Error:" + str(self.error(self.train, self.train, 2000, proj)))
+        else: 
+            print("Training Error:" + str(self.error(self.ptrain, self.train, 2000, proj)))
 
-    def predict(self, a, point):
+    
+    def predictvalid(self, proj = False):
+        if proj == False:
+            print("Validation Error:"+ str(self.error(self.valid, self.valid, 1000, proj)))
+        else:
+            print("Validation Error:"+ str(self.error(self.pvalid, self.valid, 1000, proj)))
+    
+    def predicttest(self, proj = False):
+        if proj == False:
+            print("Test Error:" + str(self.error(self.test, self.test, 1000, proj)))
+        else:
+            print("Test Error:" + str(self.error(self.ptest, self.test, 1000, proj)))
+
+    def predict(self, a, point, proj, index, orig = None):
         neighbors = dict()
+
+        if proj == False:
+            features = 784
+        else:
+            features = 20
 
         """ calculate the euclidean distance from the point and map it as a key to label 
             inside the neighbors dictionary
         """
         for i in range(len(a)-1):
-            e_dist = np.linalg.norm(a[i][:784] - point[:784])
-            neighbors[e_dist] = a[i][784]
+            e_dist = np.linalg.norm(a[i][:features] - point[:features])
+            neighbors[e_dist] = self.train[i][784]
         
         """ sort the keys/distances from the point """
         sorted_neighbors = sorted(neighbors.keys())
@@ -50,8 +82,8 @@ class KNN:
         
         """take the mode from the first k closest points"""
         predicted_label = max(set(first_k), key = first_k.count)
-
-        if predicted_label == point[784]:
+        #print(predicted_label,point[784])
+        if predicted_label == orig[index][784]:
             return True
         return False
 
--- pa1.py
+++ pa1.py
@@ -1,23 +1,85 @@
 from knn import KNN
 import pandas as pd
 import numpy as np
+import datetime
 
-model = KNN('pa1train.txt', 'pa1validate.txt', 'pa1test.txt', k = 1)
+model = KNN('pa1train.txt', 'pa1validate.txt', 'pa1test.txt', k = 3)
+
 print('k = 1')
+ti = datetime.datetime.now()
 model.predicttrain()
 model.predictvalid()
+model.predicttest()
+tf = datetime.datetime.now()
+print(tf-ti)
+
+
 
 print('k = 5')
+ti = datetime.datetime.now()
 model.setk(5)
 model.predicttrain()
 model.predictvalid()
+model.predicttest()
+tf = datetime.datetime.now()
+print(tf-ti)
 
 print('k = 9')
+ti = datetime.datetime.now()
 model.setk(9)
 model.predicttrain()
 model.predictvalid()
+model.predicttest()
+tf = datetime.datetime.now()
+print(tf-ti)
 
 print('k = 15')
+ti = datetime.datetime.now()
 model.setk(15)
 model.predicttrain()
 model.predictvalid()
+model.predicttest()
+tf = datetime.datetime.now()
+print(tf-ti)
+
+
+#projection optimization
+print("Projection Results")
+model.setp('projection.txt')
+
+print('k = 1')
+ti = datetime.datetime.now()
+model.setk(1)
+model.predicttrain(proj = True)
+model.predictvalid(proj = True)
+model.predicttest(proj = True)
+tf = datetime.datetime.now()
+print(tf-ti)
+
+
+print('k = 5')
+ti = datetime.datetime.now()
+model.setk(5)
+model.predicttrain(proj = True)
+model.predictvalid(proj = True)
+model.predicttest(proj = True)
+tf = datetime.datetime.now()
+print(tf-ti)
+
+print('k = 9')
+ti = datetime.datetime.now()
+model.setk(9)
+model.predicttrain(proj = True)
+model.predictvalid(proj = True)
+model.predicttest(proj = True)
+tf = datetime.datetime.now()
+print(tf-ti)
+
+print('k = 15')
+ti = datetime.datetime.now()
+model.setk(15)
+model.predicttrain(proj = True)
+model.predictvalid(proj = True)
+model.predicttest(proj = True)
+tf = datetime.datetime.now()
+print(tf-ti)
